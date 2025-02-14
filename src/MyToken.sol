6097aa753c5c1d79b26a7832ad36a7a13b5ecebf
HiH-DimaN <hih.diman@gmail.com>
2025-02-14 15:20:01 +0300

    added require(address(this).balance >= totalAmount, "Insufficient balance in contract");

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -2,16 +2,17 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title MyToken - Кастомный ERC20-токен протокола
  */
-contract MyToken is ERC20, Ownable {
+contract MyToken is IERC20, Ownable {
     /**
      * @dev Конструктор, создающий токен с названием MyToken и тикером MTK
      */
-    constructor() ERC20("MyToken", "MTK") Ownable(msg.sender){}
+    constructor() IERC20("MyToken", "MTK") Ownable(msg.sender){}
 
     /**
      * @dev Функция выпуска новых токенов
@@ -19,7 +20,7 @@ contract MyToken is ERC20, Ownable {
      * @param amount Количество токенов
      */
     function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount); // Выпускаем новые токены и отправляем их на адрес to
+        _safeMint(to, amount); // Выпускаем новые токены и отправляем их на адрес to
     }
 }
 
