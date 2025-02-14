a37940207c9a559509bd14bc60a5add4b699961f
HiH-DimaN <hih.diman@gmail.com>
2025-02-14 19:20:56 +0300

    changed code

--- src/TokenSale.sol
+++ src/TokenSale.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "src/MyToken.sol"; // Импорт контракта ERC20-токена
-import "src/Vault.sol"; // Импорт контракта хранилища
+import "src/Vault.sol"; // Импорт контракта хранилища 
 
 /**
  * @title TokenSale - Контракт продажи myToken с белым списком и комиссией
